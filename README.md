9a9bc8de21aeaec7f38c9d563939e94023c004c6
M Mantle <mmantlehud@googlemail.com>
2020-10-25 18:07:56 +0000

    Updated the exercises

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Instructions for running CHT2531 JavaScript Exercises
 
 1. Download or clone the repository.
-2. Unzip this.
-3. Inside this folder there are a number of files. Here are the ones we are interested in:
+2. Inside this folder there are a number of files. Here are the ones we are interested in:
 
 * *index.html*. This is the HTML file that is used in practical exercises. This HTML file uses a JavaScript file, exercises.js
 * *exercises.js*. Open this in a text editor of your choice. This is where you will write your JavaScript. It already contains some code to get you started. It also contains the exercise questions (in comments, leave these questions commented out).
@@ -12,8 +11,6 @@
 To complete the exercises this week you will need to run *index.html* from a web server.
 
 There are a number of ways to do this.
-### Using Web Server for Chrome (easy to do but blocked at the uni)
-You can add extension to the chrome browser that will run a web server for you - https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb
 
 ### Using atom-live-web-server
 We can install a web server as an atom package. Similar 'packages' are available in other text editors e.g VS Code.
@@ -25,6 +22,9 @@ We can install a web server as an atom package. Similar 'packages' are available
 * Click the install button
 * Once it is installed
 * Select packages>atom-live-server->start server
+
+### Using Web Server for Chrome (easy to do but blocked at the uni)
+You can add extension to the chrome browser that will run a web server for you - https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb
 
 ### Using HTTP-SERVER
 There is a node.js package *http-server*. To use it, do the following:
--- js/exercises.js
+++ js/exercises.js
@@ -1,34 +1,144 @@
-fetch("./data/films.json").then(function(response) {
-		return response.json();
-}).then(function(json) {
-		console.log(json)
-});
+// fetch("./data/films.json").then(function(response) {
+// 		return response.json();
+// }).then(function(json) {
+// 		console.log(json)
+// });
+
+
+// Q1) Open index.html in a browser. Open the console, note how the films data has been loaded from a JSON file.
+// a) Modify the code so that it outputs the title of each film.
+// fetch("./data/films.json").then(function(response) {
+// 		return response.json();
+// }).then(function(json) {
+// 		console.log(json)
+// 		const films = json;
+// 		films.forEach(function(film){
+// 			console.log(film.title);
+// 		})
+// });
+
+// b) Modify the code so that the full details of the second film (Winter's Bone) are displayed in the console.
+
+// fetch("./data/films.json").then(function(response) {
+// 		return response.json();
+// }).then(function(json) {
+// 		console.log(json)
+// 		const films = json;
+// 		console.log(`${films[1].title} was made in ${films[1].year}. It is ${films[1].duration} minutes in length.`);
+// });
+
+// c) Open films.json. Have a good look at the structure of the file. Add the details for another film. Modify the code so that the new film's details are displayed in the console.
+
+
+// Q2) In a text-editor open films-object.json. This contains similar data but structured in a slightly different way.
+// a) Modify your Ajax code so that it loads the data in films-object.json instead. Your code should output the list of film titles to the console.
+
+// fetch("./data/films-object.json").then(function(response) {
+// 		return response.json();
+// }).then(function(json) {
+// 		console.log(json)
+// 		const films = json.results;
+// 		films.forEach(function(film){
+// 		 			console.log(film.title);
+// 		})
+// });
+
+
+// b) Modify the code so that the genres of Winter's Bone are displayed in the console.
+
+// fetch("./data/films-object.json").then(function(response) {
+// 		return response.json();
+// }).then(function(json) {
+// 		console.log(json)
+// 		const films = json.results;
+// 		console.log(`${films[1].title} was made in ${films[1].year}. It is ${films[1].duration} minutes in length.\nGenres:`);
+// 		films[1].genres.forEach(function(genre){
+// 		 			console.log(genre);
+// 		})
+// });
+
+// b) Have a good look at the structure of the films-object.json. Add the details for another film. Modify the code so that the new film's details are displayed in the console.
+
+
+
+// Q3) Next we'll use an API instead of hard coded data
+// a) Open a new tab in a web browser enter https://www.swapi.tech/api/species. See the response you get from the SWAPI site.
+// b) Modify your code from the previous question so that you load data from the Star Wars API using this URL. To start with use a simple console.log to print the entire reponse. Then think about about how you can list just the species names.
+
+// fetch("https://www.swapi.tech/api/species").then(function(response) {
+// 		return response.json();
+// }).then(function(json) {
+// 		console.log(json)
+// 		const species = json.results;
+// 		species.forEach(function(singleSpecies){
+// 		 			console.log(singleSpecies.name);
+// 		})
+// });
+
+// c) Have a look at https://www.swapi.tech/docs and modify your code to make a request for a single species. Display some of the species details
+
+// fetch("https://www.swapi.tech/api/species/3").then(function(response) {
+// 		return response.json();
+// }).then(function(json) {
+// 		console.log(json)
+// 		const species = json.result.properties;
+// 		console.log(`Name: ${species.name}`);
+// 		console.log(`Lifespan: ${species.average_lifespan}`);
+// 		console.log(`Language: ${species.language}`);
+// });
 
 /*
-Q1) Open index.html in a browser. Open the console, note how the films data has been loaded from a JSON file
-a) Modify the code so that it outputs "Get Out was made in 2017"
-b) Open films.json. Have a good look at the structure of the file. Add the details for another film. Modify the code so that the new film's details are displayed in the console.
-c) Modify the code so that it displays the genres for the film Winter's Bone. Make sure you use a forEach loop.
+Q4) Now, instead of using the console log see if you can display Ajax data in the HTML page. Have a look at https://github.com/CHT2531/js-dom-recap/blob/master/notes.md#inserting-multiple-elements from last week for an example of using a forEach loop to insert elements into an HTML page.
 */
 
-/*
-Q2) Next we'll use an API instead of hard coded data
-a) Open a new tab in a web browser enter https://swapi.co/api/films. See the response you get from the SWAPI site.
-b) Copy the code from Q1 and modify it so that you load data from the Star Wars API instead (you might want to comment out your Q1 code for now). Use a forEach loop to display the title and release date of each film.
-c) Have a look at https://swapi.co/documentation and modify your code to make a request for different data e.g. people or planets or vehicles. Test it works.
-*/
+// fetch("https://www.swapi.tech/api/species").then(function(response) {
+// 		return response.json();
+// }).then(function(json) {
+// 		console.log(json)
+// 		const species = json.results;
+// 		const speciesFragment = document.createDocumentFragment();
+// 		species.forEach(function(singleSpecies){
+// 		    const newParagraph = document.createElement("p");
+// 		    newParagraph.textContent = `${singleSpecies.name}`;
+// 		    speciesFragment.appendChild(newParagraph);
+// 		});
+// 		const speciesDiv = document.querySelector("#species");
+// 		speciesDiv.appendChild(speciesFragment);
+// });
+
+
+
 
 /*
-Q3) Have a go with a different API
-a) Make a request to https://itunes.apple.com/search?term=Jaws. Again, copy the code from Q1 and modify it. To start with simply display the returned JSON data in the console.
-b) The iTunes API returns data for music, films and tv programmes. Can you filter the results to only get films. Display the description for each film in the console.
-*/
-
-/*
-Q4) There are two big problems with the above. First, we are duplicating the code every time we make a request. Second, we are mixing up the Ajax code with code that process the data.
-a) Create a new function, call it ajax. Put the fetch() code in this function. The ajax function should accept two arguments, a URL to request data from and a callback that will be executed when the data is returned. Have a look on the notes for an example.
+Q4) Now think about how you can make the code more re-usable.
+a) Create a new function, call it ajax. Put the fetch() code in this function. The ajax function should accept two arguments, a URL to request data from and a callback that will be executed when the data is returned. Have a look on the notes (in this repository) for an example.
 b) Make multiple calls to your ajax function for the different examples we have looked at today e.g.
 */
 
-// ajax("./data/films.json",showGenres);
-// ajax("https://swapi.co/api/films",showStarWarsFilms);
+// function ajax(url,callback)
+// {
+// 	fetch(url).then(function(response) {
+// 		return response.json();
+// 	}).then(function(json) {
+// 		callback(json)
+// 	});
+// } //end of doAjax
+//
+// const showSpecies = function(data){
+// 	console.log(data)
+// 	const species = data.results;
+// 	species.forEach(function(singleSpecies){
+// 			console.log(singleSpecies.name);
+// 	})
+// }
+//
+// const showDetails = function(data){
+// 	console.log(data)
+// 	const species = data.result.properties;
+// 	console.log(`Name: ${species.name}`);
+// 	console.log(`Lifespan: ${species.average_lifespan}`);
+// 	console.log(`Language: ${species.language}`);
+// }
+//
+// ajax("https://www.swapi.tech/api/species",showSpecies);
+// ajax("https://www.swapi.tech/api/species/3",showDetails);
