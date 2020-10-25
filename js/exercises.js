1ef56a84be9b452cb89e25018f65d5a934669604
M Mantle <mmantlehud@googlemail.com>
2020-10-25 18:17:01 +0000

    Initial commit

--- js/exercises.js
+++ js/exercises.js
@@ -7,6 +7,7 @@
 
 // Q1) Open index.html in a browser. Open the console, note how the films data has been loaded from a JSON file.
 // a) Modify the code so that it outputs the title of each film.
+
 // fetch("./data/films.json").then(function(response) {
 // 		return response.json();
 // }).then(function(json) {
@@ -28,6 +29,8 @@
 // });
 
 // c) Open films.json. Have a good look at the structure of the file. Add the details for another film. Modify the code so that the new film's details are displayed in the console.
+
+
 
 
 // Q2) In a text-editor open films-object.json. This contains similar data but structured in a slightly different way.
@@ -57,12 +60,13 @@
 // 		})
 // });
 
-// b) Have a good look at the structure of the films-object.json. Add the details for another film. Modify the code so that the new film's details are displayed in the console.
-
+// b) Have a good look at the structure of films-object.json. Add the details for another film. Modify the code so that the new film's details are displayed in the console.
 
 
 // Q3) Next we'll use an API instead of hard coded data
+
 // a) Open a new tab in a web browser enter https://www.swapi.tech/api/species. See the response you get from the SWAPI site.
+
 // b) Modify your code from the previous question so that you load data from the Star Wars API using this URL. To start with use a simple console.log to print the entire reponse. Then think about about how you can list just the species names.
 
 // fetch("https://www.swapi.tech/api/species").then(function(response) {
@@ -87,9 +91,7 @@
 // 		console.log(`Language: ${species.language}`);
 // });
 
-/*
-Q4) Now, instead of using the console log see if you can display Ajax data in the HTML page. Have a look at https://github.com/CHT2531/js-dom-recap/blob/master/notes.md#inserting-multiple-elements from last week for an example of using a forEach loop to insert elements into an HTML page.
-*/
+//Q4) Now, instead of using the console log, choose one of your examples and see if you can display Ajax data in the HTML page. Have a look at https://github.com/CHT2531/js-dom-recap/blob/master/notes.md#inserting-multiple-elements from last week for an example of using a forEach loop to insert elements into an HTML page.
 
 // fetch("https://www.swapi.tech/api/species").then(function(response) {
 // 		return response.json();
@@ -109,11 +111,9 @@ Q4) Now, instead of using the console log see if you can display Ajax data in th
 
 
 
-/*
-Q4) Now think about how you can make the code more re-usable.
-a) Create a new function, call it ajax. Put the fetch() code in this function. The ajax function should accept two arguments, a URL to request data from and a callback that will be executed when the data is returned. Have a look on the notes (in this repository) for an example.
-b) Make multiple calls to your ajax function for the different examples we have looked at today e.g.
-*/
+// Q5) Now think about how you can make the code more re-usable.
+//a) Create a new function, call it ajax. Put the fetch() code in this function. The ajax function should accept two arguments, a URL to request data from and a callback that will be executed when the data is returned. Have a look on the notes (in this repository) for an example.
+//b) Make multiple calls to your ajax function for the different examples we have looked at today e.g.
 
 // function ajax(url,callback)
 // {
